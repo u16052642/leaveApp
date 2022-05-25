b7a38624cd3bd0f9f123a2fa68681bcd7f0b0e1e
Divin <divinlonji@gmail.com>
2022-05-25 23:49:55 +0200

    update: add runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 import {defineNuxtConfig} from 'nuxt'
 
 export default defineNuxtConfig({
+        runtimeConfig: {
+            APP_NAME:process.env.APP_NAME,
+            SHORT_NAME: process.env.SHORT_NAME,
+            PORT: process.env.PORT
+        },
         css: [
             "@/assets/styles/main.scss"
         ]
