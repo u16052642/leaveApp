029309a1cc0f001b0a61a24f7b8b7db89a50e5b1
Divin Lonji <divinlonji@gmail.com>
2022-05-25 22:05:48 +0200

    update: add initial page data and styles

--- pages/[[sub]].vue
+++ pages/[[sub]].vue
@@ -1,5 +1,5 @@
 # Sub Page for dynamic pages
 <template>
-
+  <div class="sub-pages"><h2>{{$route.params.sub}}</h2></div>
 </template>
 <script setup></script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,4 +2,4 @@
 
 <template src="./views/main.html"></template>
 <script></script>
-<style></style>
+<style src="./views/main.scss"></style>
