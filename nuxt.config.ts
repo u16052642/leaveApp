8f725dff77b097e4fe895d062c28846efbf55bc4
Divin <divinlonji@gmail.com>
2022-05-26 12:07:39 +0200

    include postcss in main configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,34 @@
 import {defineNuxtConfig} from 'nuxt'
-import {ConfigEnvObject} from "./types";
+import {ConfigEnvObject} from "./types/config-env-object";
 
 // Grab all required configurations
 const CONFIG: ConfigEnvObject = {
-    APP_NAME:process.env.APP_NAME,
+    APP_NAME: process.env.APP_NAME,
     SHORT_NAME: process.env.SHORT_NAME,
     PORT: process.env.PORT,
 }
 
 export default defineNuxtConfig({
+        components: true,
         runtimeConfig: {
             ...CONFIG,
-           public: {
-              ...CONFIG
-           }
+            public: {
+                ...CONFIG
+            }
         },
         css: [
             "@/assets/styles/main.scss"
-        ]
+        ],
+
+        build: {
+            postcss: {
+                postcssOptions: {
+                    plugins: {
+                        tailwindcss: {},
+                        autoprefixer: {},
+                    },
+                },
+            }
+        }
     }
 )
