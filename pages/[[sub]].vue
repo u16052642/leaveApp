578ca6ab7c5bbded31227c69569ca3696110de5e
Divin <divinlonji@gmail.com>
2022-05-26 22:09:19 +0200

    refactor general page to error page

--- pages/[[sub]].vue
+++ pages/[[sub]].vue
@@ -1,5 +1,13 @@
 # Sub Page for dynamic pages
 <template>
-  <div class="sub-pages"><h2>{{$route.params.sub}}</h2></div>
+  <NuxtLayout name="default">
+    <section class="h-[80vh] flex justify-center items-center">
+      <h1 class="text-3xl"> Page <span class="text-red-500 ">`{{$route.params.sub}}`</span> Not found</h1>
+      <hr>
+      <NuxtLink to="/">Home page</NuxtLink>
+    </section>
+  </NuxtLayout>
 </template>
-<script setup></script>
+<script setup>
+import {NuxtLink} from "#components";
+</script>
