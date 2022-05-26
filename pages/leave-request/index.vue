7915c4639f10da5be0c517a3772537350349005a
Divin <divinlonji@gmail.com>
2022-05-26 22:09:42 +0200

    refactor general page to error page

---
+++ pages/leave-request/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <pre>{{other}}</pre>
+</template>
+<script setup>
+import {useLeaveStore} from "../../stores/leave-store";
+
+const leaveStore = useLeaveStore()
+const leaves = leaveStore.$state
+const other = computed(()=> {
+  return leaveStore.leaveData
+})
+console.log(other)
+</script>
